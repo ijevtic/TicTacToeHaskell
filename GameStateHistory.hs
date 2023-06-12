aacdc0b8a2166fbbbcda333f792ed83a6ff86d59
Ivan Jevtic <ivanjevtic501@gmail.com>
2023-06-12 17:10:16 +0200

    todo check GameStateHistory

--- GameStateHistory.hs
+++ GameStateHistory.hs
@@ -3,26 +3,31 @@ module GameStateHistory where
 import Control.Monad.State
 
 -- s je tabla
-newtype GameStateHistory s a = GameStateHistory { runGameStateHistory :: s -> (a, s) }
+newtype GameStateHistory s a = GameStateHistory { runGameStateHistory :: s -> (a, [s]) }
 
 --
 instance Functor (GameStateHistory s) where
   fmap f (GameStateHistory g) = GameStateHistory $ \tabla -> 
-    let (a, tabla') = g tabla
-    in (f a, tabla')
+    let (a, lista') = g tabla
+    in (f a, lista')
 
 -- f je funkcija u funktoru, g je vrednost u funktoru
 instance Applicative (GameStateHistory s) where
-  pure x = GameStateHistory $ \tabla -> (x, tabla)
+  pure x = GameStateHistory $ \tabla -> (x, [tabla])
   (GameStateHistory f) <*> (GameStateHistory g) =
     GameStateHistory $ \tabla ->
-      let (f1, t1) = f tabla
-          (a2, t2) = g t1
-      in (f1 a2, t2)
+      let (f1, lista') = f tabla
+          (x:xs) = lista'
+          (a2, t2) = g x
+      in (f1 a2, t2 ++ xs)
 
 instance Monad (GameStateHistory s) where
-  return = pure
-  (GameStateHistory f) >>= g = GameStateHistory $ \tabla ->
-    let (a, tabla') = f tabla
-        (GameStateHistory h) = g a
-    in h tabla'
+  return x = GameStateHistory (\s -> (x, [s]))
+  
+  (GameStateHistory h) >>= f = GameStateHistory (\s ->
+    let (a, lista') = h s
+        (GameStateHistory g) = f a
+        (b, lista'') = g (head lista')
+        -- final_list = if (head updated_list1) == (head updated_list) then updated_list else updated_list1 ++ updated_list
+        final_list = lista'' ++ lista'
+    in (b, final_list))
--- main.hs
+++ main.hs
@@ -22,6 +22,9 @@ startState = GameState Player1 startBoard
 getBoard :: GameState Field -> Board Field
 getBoard (GameState _ board) = board
 
+getPlayer :: GameState Field -> Player
+getPlayer (GameState player _) = player
+
 checkValidMove :: GameState Field -> Move -> Bool
 checkValidMove (GameState player (Board board (rows, cols))) (Move movePlayer (row, col)) = 
 	(row >= 0) && (row < rows) && (col >= 0) && (col < cols) && ((board !! row) !! col == Empty) && player == movePlayer
@@ -40,15 +43,15 @@ applyRow :: [Field] -> Int -> Move -> [Field]
 applyRow row rowNumber (Move movePlayer (rowInd, colInd)) = 
 	[if (rowNumber == rowInd) && (colInd == colNum) then (if movePlayer == Player1 then X else O) else row !! colNum | colNum <- [0..((length row) - 1)]]
 
-applyMove :: GameState Field -> Move -> GameState Field
-applyMove (GameState player (Board board (rows, cols))) move = 
+makeMove :: GameState Field -> Move -> GameState Field
+makeMove (GameState player (Board board (rows, cols))) move = 
 	GameState (nextPlayer player) (Board newBoard (rows, cols))
 	where
 		newBoard = [(applyRow (board !! numRow) numRow move) | numRow <- [0..(rows - 1)]]
 
 
 makeRoseTree :: GameState Field -> Rose (GameState Field)
-makeRoseTree gameState = Node gameState (map makeRoseTree (map (applyMove gameState) (returnAllValidMoves gameState)))
+makeRoseTree gameState = Node gameState (map makeRoseTree (map (makeMove gameState) (returnAllValidMoves gameState)))
 
 printRoseTree :: Rose (GameState Field) -> Int -> IO ()
 printRoseTree (Node gameState children) depth = do
@@ -56,15 +59,45 @@ printRoseTree (Node gameState children) depth = do
 	putStrLn $ show gameState
 	mapM_ (\child -> printRoseTree child (depth + 1)) children
 
+
+applyMove :: (Int, Int) -> GameStateOp (GameState Field) Bool
+applyMove (x, y) = GameStateOp (\gameState -> let
+	newGameState = makeMove gameState (Move (getPlayer gameState) (x, y))
+	in
+		(isGameOver newGameState, newGameState))
+
+applyMoveH :: (Int, Int) -> GameStateHistory (GameState Field) Bool
+applyMoveH (x, y) = GameStateHistory (\gameState -> let
+	newGameState = makeMove gameState (Move (getPlayer gameState) (x, y))
+	in
+		(isGameOver newGameState, [newGameState]))
+
+initialize :: GameStateHistory (GameState Field) Bool
+initialize = (GameStateHistory (\gameState -> (isGameOver gameState, [gameState]))) 
+
 -- test
 testBoard :: Board Field
 testBoard = Board [[X, O, O], [X, X, O], [Empty, X, Empty]] (3, 3)
 testState :: GameState Field
 testState = GameState Player2 testBoard
 
+testApplyMoves :: GameStateOp (GameState Field) Bool
+testApplyMoves = do
+	applyMove (0, 0)
+	applyMove (0, 1)
+	applyMove (0, 2)
+	applyMove (1, 0)
+	applyMove (1, 1)
+	applyMove (1, 2)
+	applyMove (2, 2)
+	applyMove (2, 0)
+	-- applyMove (0, 2)
 
 -- main
 main = do
+	let
+		(finished, finalState) = runGameStateOp testApplyMoves startState
+
 	printBoard testBoard
 	putStrLn "---"
 	--get board from state
@@ -72,10 +105,13 @@ main = do
 	-- printRoseTree (makeRoseTree testState) 0
 	-- printRoseTree (makeRoseTree startState) 0
 
-	putStrLn $ "num: " ++ show (leavesCount (makeRoseTree startState))
+	printBoard $ getBoard $ finalState
+	putStrLn $ show finished
+
+	-- putStrLn $ "num: " ++ show (leavesCount (makeRoseTree startState))
 
 
-	-- printBoard $ getBoard (applyMove testState (Move Player1 (0, 0)))
+	-- printBoard $ getBoard (makeMove testState (Move Player1 (0, 0)))
 	-- printMoves [move | move <- (returnAllValidMoves testState)]
 	-- putStrLn $ show $ isGameOver testState
 	-- [putStrLn move | move <- (returnAllValidMoves startState)]
